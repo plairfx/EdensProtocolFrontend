41061ef0d52769b9b25086436bd0a2420855df18
Plairfx <146370891+plairfx@users.noreply.github.com>
2025-06-25 15:33:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+ pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   async rewrites() {
     return [
       {
